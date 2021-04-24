b3c3737ae542bf10e2cb75a6133fcae527c6ee15
Kolomona Kaahanui <36248431+kolokaahanui@users.noreply.github.com>
2021-04-23 20:04:23 -1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,61 @@
+#!\Users\kolok\anaconda3\envs\py38\python3.8
+
+# import tkinter
+# from tkinter import *
+import string
+from nltk.corpus import stopwords
+import pandas as pd
+import tkinter as tk
+import time
+from  nlpFunction import nlpFunctions
+
+
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.toSummarize = ""
+        self.entry = tk.Entry(self)  # options = []
+
+        self.entry['textvariable'] = self.toSummarize
+        self.entry.pack(side="top")
+        self.summarizeButton = tk.Button(self)
+        self.summarizeButton["text"] = "Translate"
+        self.summarizeButton["command"] = self.summarize
+        self.summarizeButton.pack(side="top")
+
+        #self.summary = ""
+        self.summaryMessage = tk.Message(self)
+        #self.summaryLabel["text"] = self.summary
+        # self.summaryLabel["height"] = 10
+        # self.summaryLabel["width"] = 20
+        self.summaryMessage.pack(expand=True)
+
+        #self.quit = tk.Button(self, text="QUIT", fg="red",
+                              #command=self.master.destroy)
+        #self.quit.pack(side="bottom")
+
+    def _summarize(self):
+        #self.summary = self.toSummarize
+        print("hw")
+        print(self.summary)
+        print(self.entry['textvariable'])
+
+    def summarize(self):
+        inp = self.entry.get()  # get(1.0, "end-1c")
+        inp = nlpFunction(inp)
+        # self.summaryLabel.config(text="one moment...")
+        # time.sleep(1)
+        self.summaryMessage.config(text=inp)
+
+
+root = tk.Tk()
+root.geometry("600x400")
+app = Application(master=root)
+app.mainloop()
+
+
---
+++ nlpFunction.py
@@ -0,0 +1,124 @@
+#!\Users\kolok\anaconda3\envs\py38\python3.8
+"""
+ROFL
+
+"""
+import string
+from nltk.corpus import stopwords
+import pandas as pd
+
+def nlpFunction(someText):
+
+    sentences = someText.split(".")
+    sentences2 = []
+    for sentence in sentences:
+        sentences2.append(sentence.rstrip())
+
+    # Whitespace
+    sentences3 = []
+    for sentence in sentences2:
+        sentences3.append(sentence.strip())
+
+    print("    .")
+    # Empty strings
+    sentences4 = []
+    for sentence in sentences3:
+        if len(sentence) > 0:
+            sentences4.append(sentence)
+
+    sentences = sentences4
+
+    import string
+    nopunc = string.punctuation
+    print("    .")
+    # Remove punctuation
+    sentencesA = []
+    for sentence in sentences:
+        temp = ""
+        for char in sentence:
+            if char not in nopunc:
+                if char != "-":  # toggle
+                    temp += char
+        sentencesA.append(temp)
+
+    # Lowercase
+    sentencesB = []
+    for sentence in sentencesA:
+        sentencesB.append(sentence.lower())
+
+    sentences = sentencesB
+
+    words = []
+    print("    .")
+    from nltk.corpus import stopwords
+
+    for sentence in sentences:
+        temp = sentence.split()
+        for word in temp:
+            if word not in stopwords.words('english'):
+                words.append(word)
+
+    import pandas as pd
+    print("    .")
+    freqDict = {}
+    for word in words:
+        if word not in freqDict:
+            freqDict[word] = 0
+        else:
+            freqDict[word] += 1
+
+    setWords = set(words)
+    setWordsList = list(setWords)
+
+    df = pd.DataFrame(data=[[i, freqDict[i]] for i in setWordsList], columns=["Word", "Freq"])
+
+    df.sort_values(by="Freq", ascending=False, inplace=True)
+
+    df['wFreq'] = df['Freq'].apply(lambda x: x / 5)
+    print("    .")
+    # sum of weighted frequencies
+    swf = []
+
+    swfDict = {}
+    wordArray = df['Word']
+    wFreq = df['wFreq']
+
+    for i in range(len(wordArray)):
+        swfDict[wordArray[i]] = wFreq[i]
+
+    swfList = []
+    print("    .")
+    for sentence in sentences:
+        temp = sentence.split()
+        score = 0
+        for i in temp:
+            if i in swfDict:
+                score += swfDict[i]
+        swfList.append(score)
+
+    df2 = pd.DataFrame(data=[[sentences4[i], swfList[i]] for i in range(len(swfList))], columns=["Sentence", "Score"])
+
+    df2.sort_values(by="Score", ascending=False, inplace=True)
+
+    df2.reset_index(drop=True, inplace=True)
+    print("    .")
+
+    print("    .")
+
+    def numLines(num):
+        temp = ""
+        for i in df2['Sentence'].head(num):
+            temp += i
+            temp += "."
+            temp += " "
+
+        #print(temp)
+        return temp
+
+    #hmLines = int(input("How many lines should we summarize to?: "))
+    hmLines = 3
+    print("")
+    out = numLines(hmLines)
+
+    return out
+
